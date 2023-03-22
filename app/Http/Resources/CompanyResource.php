a7ab6433bf7281189ce63b890bd106ee68949974
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-22 12:35:50 +0000

    add employee and company resource classes

---
+++ app/Http/Resources/CompanyResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'logo' => $this->logo,
+            'website' => $this->website,
+            'employees' => $this->whenLoaded('employees', function () {
+                return EmployeeResource::collection($this->employees);
+            }),
+        ];
+    }
+}
