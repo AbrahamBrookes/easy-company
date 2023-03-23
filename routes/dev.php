c298527ba6a5ee9201d09c3eaf6d5f96e729dee0
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-23 12:37:33 +0000

    added mail preview dev route

--- routes/dev.php
+++ routes/dev.php
@@ -1,6 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Notification;
+
 /**
- * These routes are protected by the DevEnvironmentOnly middleware, registered in the 
+ * These routes are protected by the DevEnvironmentOnly middleware, registered in the
  * RouteServiceProvider and Http Kernel. These routes are only visible if the app env
  * is set to 'local'.
 */
 
+Route::prefix('mail')->group(function () {
+    Route::get('/your-company-has-been-added', function () {
+        $company = \App\Models\Company::factory()->create();
+
+        return new App\Mail\YourCompanyHasBeenAdded($company);
+    });
+});
