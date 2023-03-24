57817720bd2cfc38b43920dbfd9f368f8ff103c2
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-24 23:18:53 +0000

    add companies fixtures

---
+++ stories/Fixtures/companies.js
@@ -0,0 +1,72 @@
+export default [
+	{
+		"id":1,
+		"name":"Casper PLC",
+		"email":"malika.leffler@example.org",
+		"logo":"https://via.placeholder.com/640x480.png/0066cc?text=eos",
+		"website":"http://www.price.com/quo-officiis-facilis-enim-dolores-harum"
+	},
+	{
+		"id":2,
+		"name":"Beier, Dickens and Zemlak",
+		"email":"verner.dickens@example.org",
+		"logo":"https://via.placeholder.com/640x480.png/00bb44?text=corrupti",
+		"website":"https://www.lesch.com/voluptates-eum-et-quaerat-sunt-maiores-saepe-sit"
+	},
+	{
+		"id":3,
+		"name":"Bradtke, Dickinson and Bruen",
+		"email":"shania45@example.org",
+		"logo":"https://via.placeholder.com/640x480.png/0066cc?text=placeat",
+		"website":"http://www.sipes.com/"
+	},
+	{
+		"id":4,
+		"name":"Mraz, Jacobi and Ritchie",
+		"email":"spencer.coleman@example.com",
+		"logo":"https://via.placeholder.com/640x480.png/0077ee?text=hic",
+		"website":"http://www.casper.com/id-nam-fugiat-voluptate"
+	},
+	{
+		"id":5,
+		"name":"Smith Group",
+		"email":"toni05@example.com",
+		"logo":"https://via.placeholder.com/640x480.png/005588?text=error",
+		"website":"https://harris.com/excepturi-qui-molestiae-eveniet-tenetur-voluptas-aperiam.html"
+	},
+	{
+		"id":6,
+		"name":"Zboncak, Schmidt and Lynch",
+		"email":"jenifer33@example.net",
+		"logo":"https://via.placeholder.com/640x480.png/0011bb?text=quas",
+		"website":"http://reinger.com/"
+	},
+	{
+		"id":7,
+		"name":"Runolfsdottir Group",
+		"email":"miller.archibald@example.net",
+		"logo":"https://via.placeholder.com/640x480.png/00ffee?text=consequuntur",
+		"website":"https://www.hill.com/vel-saepe-ipsam-aut-tenetur-nisi-est-et-pariatur"
+	},
+	{
+		"id":8,
+		"name":"Daniel Inc",
+		"email":"kali.barrows@example.com",
+		"logo":"https://via.placeholder.com/640x480.png/00cc44?text=est",
+		"website":"https://christiansen.com/deleniti-qui-blanditiis-expedita-nobis-ea-consectetur-sed.html"
+	},
+	{
+		"id":9,
+		"name":"Krajcik Group",
+		"email":"jamaal.harvey@example.org",
+		"logo":"https://via.placeholder.com/640x480.png/00aa77?text=fugit",
+		"website":"http://www.wolff.com/est-perferendis-possimus-quidem-ea-aut-possimus.html"
+	},
+	{
+		"id":10,
+		"name":"Padberg-Daugherty",
+		"email":"reilly.brakus@example.net",
+		"logo":"https://via.placeholder.com/640x480.png/00aacc?text=repellendus",
+		"website":"http://www.gutmann.com/laboriosam-quae-et-sed-vel-veritatis-omnis"
+	}
+]
