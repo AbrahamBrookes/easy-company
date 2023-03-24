b4ce9772de6b589fc549fc12b52d8d8737a871de
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-24 10:26:51 +0000

    add lateral version of ApplicationLogo

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -22,7 +22,8 @@ const showingNavigationDropdown = ref(false);
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                                        :layout="'lateral'"
+                                        class="block h-20 w-48"
                                     />
                                 </Link>
                             </div>
