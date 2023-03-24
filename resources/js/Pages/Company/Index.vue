3460d5c6d163b2e89b18eafb7c0624fb479b7f9e
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-24 23:04:45 +0000

    add company index page

---
+++ resources/js/Pages/Company/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ModelList from '@/Components/ModelList'
+
+const props = defineProps({
+    companies: {
+        type: Array,
+        required: true,
+    },
+})
+
+</script>
+
+<template>
+    <ModelList :items="companies" />
+</template>
