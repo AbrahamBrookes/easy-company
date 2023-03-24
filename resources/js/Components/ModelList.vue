d1acc3831a959b98b32f34a0638948cf9ac5b2f8
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-24 23:34:55 +0000

    add explainer comment to ModelList.vue

--- resources/js/Components/ModelList.vue
+++ resources/js/Components/ModelList.vue
@@ -1,4 +1,9 @@
 <script setup>
+/**
+ * This component is meant as a base for model lists around the app.
+ * You'll likely never use this component directly, rather use is as
+ * a base for your own model list components.
+ */
 import ModelListItem from '@/Components/ModelListItem'
 
 const props = defineProps({
