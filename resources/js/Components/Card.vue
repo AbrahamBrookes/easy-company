93ce23f5a3eb1263ce2f67055e2eda5f20d89ab5
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-24 12:51:36 +0000

    add Card component

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        class="
+            bg-white dark:bg-grey-400
+            sm:rounded-md
+            shadow-sm
+            px-6 py-5
+        "
+    >
+        <slot />
+    </div>
+</template>
