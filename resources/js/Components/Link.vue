48a03565d13937b1cbb0c528bc7348bf1c505d88
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-24 10:25:38 +0000

    add Link wrapper component

---
+++ resources/js/Components/Link.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+        <Link class="
+            underline text-indigo-800 inline
+        ">
+            <slot />
+        </Link>
+</template>
