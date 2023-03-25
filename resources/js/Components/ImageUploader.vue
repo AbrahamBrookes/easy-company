3b5c926ed65e81d63b4ec881ed75f9c4703a7c4d
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-25 00:54:07 +0000

    add base of ImageUploader

---
+++ resources/js/Components/ImageUploader.vue
@@ -0,0 +1,34 @@
+<script setup>
+/**
+ * The ImageUploader is a combination of a file input and a preview image.
+ *
+ * We assume that the surrounding context uses an inertia form helper to
+ * manage the image upload within the form of some model edit page. When
+ * we emit the image, we expect the parent to handle the upload.
+ */
+import { ref, computed } from 'vue'
+
+const image = ref(null)
+// if we have no image, we use a placeholder
+const placeholder = 'https://via.placeholder.com/250/edeffb?text=Minimum+100x100'
+// a display image to show in the preview
+const displayImage = computed(() => image.value || placeholder)
+
+const handleFileChange = (event) => {
+    const file = event.target.files[0]
+    $emit('file', file)
+}
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <img :src="displayImage" class="w-60 h-60 rounded-lg mb-0" />
+        <input
+            type="file"
+            @change="handleFileChange"
+            class="opacity-0 absolute w-full h-full inset-0 cursor-pointer"
+        />
+        <p class="text-sm text-indigo-400 dark:text-indigo-600 italic">Click to change image</p>
+    </div>
+</template>
