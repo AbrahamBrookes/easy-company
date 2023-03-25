5947c37a2c8f8eb4c646d8b1bcacf2c629163957
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-25 06:29:04 +0000

    allow logo nullable on companies table

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use \Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class Company extends Model
 {
@@ -21,5 +23,45 @@ class Company extends Model
     public function employees()
     {
         return $this->hasMany(Employee::class);
+    }
+
+    /**
+     * Save an uploaded logo file to storage, and return its path.
+     */
+    public function saveLogo(UploadedFile $file)
+    {
+        // force create any directories that don't exist
+        Storage::makeDirectory('public/companies/' . $this->id . '/logo', 0644, true);
+        // store the file,
+        $path = $file->store('public/companies/' . $this->id . '/logo', 'local', true);
+
+        // update the model with the new path
+        $this->update([
+            'logo' => $path,
+        ]);
+    }
+
+    /**
+     * Delete the logo file from storage.
+     */
+    public function deleteLogo()
+    {
+        if ($this->logo) {
+            Storage::delete($this->logo);
+        }
+    }
+
+    /**
+     * The logo is either a file path to storage or a url.
+     * If it's a url, just return it.
+     * If it's a file path, return the url to the file.
+     */
+    public function getLogoUrlAttribute()
+    {
+        if (isset(parse_url($this->logo)['host'])) {
+            return $this->logo;
+        } else {
+            return config('app.url') . Storage::url($this->logo);
+        }
     }
 }
