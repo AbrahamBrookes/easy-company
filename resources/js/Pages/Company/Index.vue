7d18a8c070bd4f2deafa46cd249506460041b171
Abraham Brookes <abrahambrookes@gmail.com>
2023-03-25 22:01:07 +0000

    add create buttons to employee and company index

--- resources/js/Pages/Company/Index.vue
+++ resources/js/Pages/Company/Index.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { router } from '@inertiajs/vue3'
 import ModelList from '@/Components/Company/ModelList'
 import Content from '@/Components/Content'
+import Card from '@/Components/Elements/Card'
+import H2 from '@/Components/Elements/H2'
+import PrimaryButton from '@/Components/Elements/PrimaryButton'
 
 const props = defineProps({
     companies: {
@@ -9,12 +13,29 @@ const props = defineProps({
     },
 })
 
+function create(){
+    router.visit(route('companies/create'))
+}
+
 </script>
 
 <template>
     <Content>
-        <ModelList
-            :items="companies.data"
-        />
+        <div class="flex w-full justify-between items-center mb-4">
+            <H2>
+                Companies
+            </H2>
+            <PrimaryButton
+                @click="create"
+            >
+                Create new Company
+            </PrimaryButton>
+        </div>
+
+        <Card>
+            <ModelList
+                :items="companies.data"
+            />
+        </Card>
     </Content>
 </template>
--- resources/js/Pages/Employee/Index.vue
+++ resources/js/Pages/Employee/Index.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { router } from '@inertiajs/vue3'
 import ModelList from '@/Components/Employee/ModelList'
 import Content from '@/Components/Content'
+import Card from '@/Components/Elements/Card'
+import H2 from '@/Components/Elements/H2'
+import PrimaryButton from '@/Components/Elements/PrimaryButton'
 
 const props = defineProps({
     employees: {
@@ -9,12 +13,30 @@ const props = defineProps({
     },
 })
 
+function create(){
+    router.visit(route('employees/create'))
+}
+
+
 </script>
 
 <template>
     <Content>
-        <ModelList
-            :items="employees.data"
-        />
+        <div class="flex w-full justify-between items-center mb-4">
+            <H2>
+                Employees
+            </H2>
+            <PrimaryButton
+                @click="create"
+            >
+                Create new Employee
+            </PrimaryButton>
+        </div>
+
+        <Card>
+            <ModelList
+                :items="employees.data"
+            />
+        </Card>
     </Content>
 </template>
